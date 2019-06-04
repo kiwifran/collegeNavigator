9e1841a1e617346928820932e2f4ccd5a86da671
andrewthamcc <andrew.tham.cc@gmail.com>
2019-06-04 16:24:16 -0400

    api added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react'
 import { HashRouter, Route, Switch } from 'react-router-dom';
+import axios from 'axios';
 
 import './styles/style.scss';
 
 import Nav from './components/Nav.js';
-import Search from './components/Search.js';
 import Notes from './components/Notes.js';
 
 
 class App extends Component {
-  
+  constructor() {
+    super();
+
+    this.state = {
+      university: '4bf58dd8d48988d1ae941735',
+      college: '4bf58dd8d48988d1a2941735',
+      trade: '4bf58dd8d48988d1ad941735'
+    }
+  }
+
+  handleClick = () => {
+    axios.get('https://api.foursquare.com/v2/venues/search', {
+      params: {
+
+        // THIS IS ANDREW'S API KEY
+        client_id: 'JYPGSEYBVO44BBH553GNVMI1OCUCDFTFZUS2H0X5JLMCMOVY',
+        client_secret: 'XBVDWZV2LUQS3RUWTMJJQVJMKZY5HACXHKCGT4ZXVHHXX5K3',
+        v: 20190101,
+        near: 'Toronto',
+
+        // categoryID hardcoded currently for university
+        categoryId: this.state.university,
+        radius: 250000,
+        intent: 'checkin',
+        limit: 50,
+        query: 'university'
+      }
+    }).then(result => {
+      // this returns an array of 50 schools
+      const schoolsList = result.data.response.venues
+
+      console.log(schoolsList)
+
+    })
+  }  
+
   render() {
     return (
       <HashRouter>
         <Nav />
+        <button onClick={this.handleClick}>Get School List</button>
+
         <Switch>
-          <Route exact path='/' component={Search} />
           <Route path='/notes' component={Notes} />
         </Switch>
       </HashRouter>
