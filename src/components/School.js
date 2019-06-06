0229eee9b173e43969c074e1a97e5da30d5fbf71
FrankieWang <francescafranci@outlook.com>
2019-06-06 18:32:22 -0400

    fix schooldetails for a little bit

--- src/components/School.js
+++ src/components/School.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import SchoolDetails from "./SchoolDetails.js"
+import axios from 'axios';
 
 class School extends Component {
   constructor() {
     super();
     this.state = {
-      bookmarked: false
+      bookmarked: false,
+      schoolMoreInfo:{},
+      isModalOpen: false,
+
     }
   }
 
@@ -12,8 +17,61 @@ class School extends Component {
     this.props.setBookmarkState(id)
   }
   
+  moreInfo = (id) => {
+    axios.get(`https://api.foursquare.com/v2/venues/${id}`, {
+      params: {
+        // THIS IS ANDREW'S API KEY 
+        // client_id: 'H5KTLRAURYRNV350DJHQNDMORMYO0GN3KP12FFUMTXWI2XCO',
+        // client_secret: 'XIFGNZS4EZHSNJD2U45HSDMBUGTW4TEF5RKK3BZBW3V4R5NB',
+
+        //then it's Frankie's
+        // client_id: 'JYPGSEYBVO44BBH553GNVMI1OCUCDFTFZUS2H0X5JLMCMOVY',
+        // client_secret: 'XBVDWZV2LUQS3RUWTMJJQVJMKZY5HACXHKCGT4ZXVHHXX5K3',
+
+        //Jasmine
+        client_id: 'CJ4XSVLTY1JQ3SPHZV00JRFHCFAIXO5Y1HAASCOPGXG3URXV',
+        client_secret: 'WUABEMQMIZBR2LSKFURNG4NY2IF10CDAPAX0QSWWYGFGV2ZP',
+        v: 20190101,
+      }
+    }).then(res => {
+      console.log(res)
+      // console.log(res.data.response.venue);
+      const schoolMoreInfo = res.data.response.venue;
+
+      this.setState({
+        // schoolPhotoUrl  :`${schoolDetails.bestPhoto.prefix}500x500${schoolDetails.bestPhoto.suffix}`,
+        // schoolName :schoolDetails.name,
+        // schoolAddress: schoolDetails.location.formattedAddress,
+        schoolMoreInfo: schoolMoreInfo,
+        isModalOpen: true
+
+      })
+    }).catch((error) => {
+      // Error
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+    });
+  }
+
   render() {
     return (
+      <Fragment>
+
+      
       <div className="schoolsList container" aria-live="polite">
         {this.props.schoolsList.length ? this.props.schoolsList.map(school => {
           const { id, name: schoolName, location } = school;
@@ -27,11 +85,13 @@ class School extends Component {
               <div className="bookmarkOff">
                 <button onClick={() => { this.addNote(id) }}><i className="far fa-bookmark"></i></button>
               </div>
-              <button onClick={() => this.props.userSelectSchoolId(id)}>More Info</button>
+              <button onClick={() => this.moreInfo(id)}>More Info</button>
             </div>
           )
         }) : null}
       </div>
+      <SchoolDetails schoolMoreInfo={this.state.schoolMoreInfo}/>
+      </Fragment>
     )
   }
 }
