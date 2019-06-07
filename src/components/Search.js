384c876cbd95ec03687a80ba577a74ca94383a42
andrewthamcc <andrew.tham.cc@gmail.com>
2019-06-07 14:14:19 -0400

    refactored radio inputs and form - search

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,191 +2,181 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import firebase from './firebase.js';
 import School from './School.js';
-import SchoolDetails from "./SchoolDetails.js"
-
+import SchoolDetails from './SchoolDetails.js';
 
 class Search extends Component {
-    constructor() {
-        super()
-        this.state = {
-            university: false,
-            college: false,
-            tradeSchool: false,
-            userInstitute: '',
-            userInput: '',
-            schoolsList: [],
-            selectSchoolId: "",
+  constructor() {
+    super();
+    this.state = {
+      userInput: '',
+      schoolsList: [],
+      selectSchoolId: '',
+      institution: ''
+    };
+  }
+
+  // calls the API
+  apiCall = () => {
+    axios
+      .get('https://api.foursquare.com/v2/venues/search', {
+        params: {
+          client_id: 'JYPGSEYBVO44BBH553GNVMI1OCUCDFTFZUS2H0X5JLMCMOVY',
+          client_secret: 'XBVDWZV2LUQS3RUWTMJJQVJMKZY5HACXHKCGT4ZXVHHXX5K3',
+          v: 20190101,
+          near: this.state.userInput,
+          categoryId: this.state.institution,
+          radius: 25000,
+          intent: 'checkin',
+          limit: 50
         }
-    }
-    userSelectSchoolId=(id)=>{
-        console.log('search', id);
-        this.setState({
-            selectSchoolId:id,
-        })
-    }
-    // calls the API
-    apiCall = () => {
-        axios.get('https://api.foursquare.com/v2/venues/search', {
-            params: {
+      })
+      .then(result => {
+        const regex = /centre|center|park|building|pool|hall|office of le president|division of|department|campus|residence|faculty|campus|public|room/i;
+        const schoolsList = result.data.response.venues;
+        const filteredSchoolList = [];
 
-                // THIS IS ANDREW'S API KEY
-                client_id: 'JYPGSEYBVO44BBH553GNVMI1OCUCDFTFZUS2H0X5JLMCMOVY',
-                client_secret: 'XBVDWZV2LUQS3RUWTMJJQVJMKZY5HACXHKCGT4ZXVHHXX5K3',
-                v: 20190101,
-                near: this.state.userInput,
-
-                // categoryID hardcoded currently for university
-                categoryId: this.state.userInstitute,
-                radius: 25000,
-                intent: 'checkin',
-                limit: 50,
-                // query: 'university'
-            }
-        }).then(result => {
-            // this returns an array of 50 schools
-            const regex = /centre|center|park|building|pool|hall|office of le president|division of|department|campus|residence|faculty|campus|public|room/i
-            const schoolsList = result.data.response.venues
-            const filteredSchoolList = [];
-
-            console.log(schoolsList)
-
-            schoolsList.forEach(key => {
-                const name = key.categories[0].shortName
-                if ((name === 'University' || name === 'Community College' || name === 'Trade School') && !(regex.test(key.name)) && (key.location.address !== undefined)) {
-                    filteredSchoolList.push(key)
-                }
-            })
-            console.log(filteredSchoolList)
-
-            this.setState({
-                schoolsList: filteredSchoolList,
-            })
-        }).catch((error) => {
-            // Error
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                console.log(error.response.data);
-                console.log(error.response.status);
-                console.log(error.response.headers);
-            } else if (error.request) {
-                // The request was made but no response was received
-                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                // http.ClientRequest in node.js
-                console.log(error.request);
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                console.log('Error', error.message);
-            }
-            console.log(error.config);
+        schoolsList.forEach(key => {
+          const name = key.categories[0].shortName;
+          if (
+            (name === 'University' || name === 'Community College' || name === 'Trade School') &&
+            !regex.test(key.name) &&
+            key.location.address !== undefined
+          ) {
+            filteredSchoolList.push(key);
+          }
         });
-    }
 
-    handleChange = (event) => {
+        console.log(filteredSchoolList);
+
         this.setState({
-            userInput: event.currentTarget.value
-        })
-    }
-
-    getInstitute = (event) => {
-        this.setState({
-            userInstitute: event.target.getAttribute('value')
-        })
-    }
-
-    // bookmarks into firebase the selected school
-    setBookmarkState = (id) => {
-        console.log(id)
-
-        this.state.schoolsList.map((school) => {
-            if (school.id === id) {
-                const dbRef = firebase.database().ref();
-                dbRef.push({
-                    name: school.name,
-                    address: school.location.address,
-                    id: school.id
-                })
-            }
+          schoolsList: filteredSchoolList
         });
-    }
-
-    // ALL THIS IS FOR RADIO BUTTONS 
-    handleClick = (event) => {
-        if (event.currentTarget.getAttribute('value') === '4bf58dd8d48988d1ae941735') {
-            this.setState({
-                university: true,
-                college: false,
-                tradeSchool: false
-            })
-        } else if (event.currentTarget.getAttribute('value') === '4bf58dd8d48988d1a2941735') {
-            this.setState({
-                university: false,
-                college: true,
-                tradeSchool: false
-            })
-        } else if (event.currentTarget.getAttribute('value') === '4bf58dd8d48988d1ad941735') {
-            this.setState({
-                university: false,
-                college: false,
-                tradeSchool: true
-            })
+      })
+      .catch(error => {
+        // Error
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
         }
-        this.getInstitute(event)
-    }
+        console.log(error.config);
+      });
+  };
 
-    render() {
-        return (
-            <div className='searchContainer wrapper'>
-                <div className='radioButtons'>
-                    {this.state.university ?
-                        <div className='radioButton selected'>
-                            <input type='radio' name='institution' id='university' value='4bf58dd8d48988d1ae941735' selected></input>
-                            <label htmlFor="university">University</label>
-                        </div>
-                        :
-                        <div className='radioButton'>
-                            <input type='radio' name='institution' id='university' value='4bf58dd8d48988d1ae941735' onClick={this.handleClick}></input>
-                            <label htmlFor="university">University</label>
-                        </div>}
+  // bookmarks into firebase the selected school
+  setBookmarkState = id => {
+    console.log(id);
 
-                    {this.state.college ?
-                        <div className='radioButton selected'>
-                            <input type='radio' name='institution' id='college' value='4bf58dd8d48988d1a2941735' selected></input>
-                            <label htmlFor="college">College</label>
-                        </div>
-                        :
-                        <div className='radioButton'>
-                            <input type='radio' name='institution' id='college' value='4bf58dd8d48988d1a2941735' onClick={this.handleClick}></input>
-                            <label htmlFor="college">College</label>
-                        </div>}
+    this.state.schoolsList.map(school => {
+      if (school.id === id) {
+        const dbRef = firebase.database().ref();
+        dbRef.push({
+          name: school.name,
+          address: school.location.address,
+          id: school.id
+        });
+      }
+    });
+  };
 
-                    {this.state.tradeSchool ?
-                        <div className='radioButton selected'>
-                            <input type='radio' name='institution' id='tradeSchool' value='4bf58dd8d48988d1ad941735' selected></input>
-                            <label htmlFor="tradeSchool">Trade School</label>
-                        </div>
-                        :
-                        <div className='radioButton'>
-                            <input type='radio' name='institution' id='tradeSchool' value='4bf58dd8d48988d1ad941735' onClick={this.handleClick}></input>
-                            <label htmlFor="tradeSchool">Trade School</label>
-                        </div>}
-                </div>
-                <input type="text" name="search" id="search" onChange={this.handleChange} />
-                <button onClick={this.apiCall}>Get School List</button>
+  // handle change for form
+  handleChange = e => {
+    this.setState({
+        [e.target.name]: e.target.value
+      });
+  };
 
-                <School
-                    schoolsList={this.state.schoolsList}
-                    setBookmarkState={this.setBookmarkState}
-                    userSelectSchoolId={this.userSelectSchoolId}
-                />
+  // handle submit of form
+  handleSubmit = e => {
+    // requires error handling of things not selected
 
-                <SchoolDetails
-                    schoolId={this.state.selectSchoolId}
-                />
+    this.apiCall();
 
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <div className="wrapper">
+        <form onSubmit={this.handleSubmit}>
+          <div className="searchContainer">
+            <div className="radioButtons">
+              <input
+                type="radio"
+                name="institution"
+                id="university"
+                className="radioButtonDot"
+                value="4bf58dd8d48988d1ae941735"
+                onChange={this.handleChange}
+                checked={this.state.institution === '4bf58dd8d48988d1ae941735'}
+              />
+              <label className="radioButtonLabel" htmlFor="university">
+                University
+              </label>
+
+              <input
+                type="radio"
+                name="institution"
+                id="college"
+                className="radioButtonDot"
+                value="4bf58dd8d48988d1a2941735"
+                onChange={this.handleChange}
+                checked={this.state.institution === '4bf58dd8d48988d1a2941735'}
+              />
+              <label className="radioButtonLabel" htmlFor="college">
+                College
+              </label>
+
+              <input
+                type="radio"
+                name="institution"
+                id="trade"
+                className="radioButtonDot"
+                value="4bf58dd8d48988d1ad941735"
+                onChange={this.handleChange}
+                checked={this.state.institution === '4bf58dd8d48988d1ad941735'}
+              />
+              <label className="radioButtonLabel" htmlFor="trade">
+                Trade School
+              </label>
             </div>
-        )
-    }
+
+            <label htmlFor="search">Schools Near:</label>
+            <input
+              type="text"
+              name="userInput"
+              id="search"
+              onChange={this.handleChange}
+              value={this.state.userInput}
+            />
+
+            <label htmlFor="submit" className="visuallyHidden">
+              Submit Search
+            </label>
+            <input type="submit" id="submit" value="Get Schools List" />
+          </div>
+        </form>
+
+        <School
+          schoolsList={this.state.schoolsList}
+          setBookmarkState={this.setBookmarkState}
+          userSelectSchoolId={this.userSelectSchoolId}
+        />
+
+        <SchoolDetails schoolId={this.state.selectSchoolId} />
+      </div>
+    );
+  }
 }
 
-export default Search
+export default Search;
