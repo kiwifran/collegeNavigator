d9f39299b235a8191880c25e4f00ac06903f81f5
David Thavixay <david.thavixay@gmail.com>
2019-06-10 14:51:05 -0400

    added some accessibility

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,14 +6,14 @@ function Nav() {
     <header>
         <nav className="nav">
           <div className="wrapper">
-            <NavLink to="/" className="navLink"><h1>College Navigator</h1></NavLink>
+            <NavLink to="/" className="navLink" tabIndex='1'><h1>College Navigator</h1></NavLink>
             <ul className="navList">
-              <li className="navItem"><NavLink to="/" className="navLink">Search</NavLink></li>
-              <li className="navItem"><NavLink to="/notes" className="navLink">Bookmarks</NavLink></li>
+            <li className="navItem"><NavLink to="/" className="navLink" tabIndex='2'>Search</NavLink></li>
+            <li className="navItem"><NavLink to="/notes" className="navLink" tabIndex='3'>Bookmarks</NavLink></li>
             </ul>
             <ul className="hiddenNavList">
-            <li className="navItem"><NavLink to="/" className="navLink" aria-label="search"><i class="fas fa-search"></i></NavLink></li>
-            <li className="navItem"><NavLink to="/notes" className="navLink"><i class="fas fa-clipboard-list"></i></NavLink></li>
+            <li className="navItem"><NavLink to="/" className="navLink" aria-label="search" tabIndex='2'><i class="fas fa-search"></i></NavLink></li>
+            <li className="navItem"><NavLink to="/notes" className="navLink" tabIndex='3'><i class="fas fa-clipboard-list"></i></NavLink></li>
             </ul>
           </div>
         </nav>
